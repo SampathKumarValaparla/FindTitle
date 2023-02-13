fb9b445ec8024808a509feb6c5bb0fa34e0b5d07
SampathKumarValaparla <122368678+SampathKumarValaparla@users.noreply.github.com>
2023-02-13 15:57:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,128 +1,93 @@
-console.log("Hello world");
-const moviesOriginalList = [
-    { title: "The Shawshank Redemption", genre: "Drama" },
-    { title: "The Godfather", genre: "Crime" },
-    { title: "The Godfather: Part II", genre: "Crime" },
-    { title: "The Dark Knight", genre: "Action" },
-    { title: "12 Angry Men", genre: "Drama" },
-    { title: "Schindler's List", genre: "Drama" },
-    { title: "The Lord of the Rings: The Return of the King", genre: "Adventure" },
-    { title: "Pulp Fiction", genre: "Crime" },
-    { title: "The Good, the Bad and the Ugly", genre: "Western" },
-    { title: "Fight Club", genre: "Drama" },
-    { title: "Forrest Gump", genre: "Drama" },
-    { title: "Inception", genre: "Action" },
-    { title: "The Lord of the Rings: The Fellowship of the Ring", genre: "Adventure" },
-    { title: "Star Wars: Episode V - The Empire Strikes Back", genre: "Action" },
-    { title: "The Lord of the Rings: The Two Towers", genre: "Adventure" },
-    { title: "The Matrix", genre: "Action" },
-    { title: "Goodfellas", genre: "Crime" },
-    { title: "One Flew Over the Cuckoo's Nest", genre: "Drama" },
-    { title: "Seven Samurai", genre: "Adventure" },
-    { title: "Se7en", genre: "Crime" },
-    { title: "City of God", genre: "Crime" },
-    { title: "The Silence of the Lambs", genre: "Thriller" },
-    { title: "It's a Wonderful Life", genre: "Drama" },
-    { title: "Life is Beautiful", genre: "Comedy" },
-    { title: "The Usual Suspects", genre: "Crime" },
-    { title: "Léon: The Professional", genre: "Action" },
-    { title: "Spirited Away", genre: "Animation" },
-    { title: "Saving Private Ryan", genre: "Drama" },
-    { title: "Interstellar", genre: "Adventure" },
-    { title: "The Green Mile", genre: "Drama" },
-    { title: "The Prestige", genre: "Drama" },
-    { title: "The Intouchables", genre: "Comedy" },
-    { title: "The Lion King", genre: "Animation" },
-    { title: "The Pianist", genre: "Drama" },
-    { title: "The Departed", genre: "Crime" },
-    { title: "Whiplash", genre: "Drama" },
-    { title: "Gladiator", genre: "Action" }
-  ]
-  let movies = [];
-  
-  //BONUS: local storage 
-  localStorage.setItem('movieListToSave',JSON.stringify(moviesOriginalList));
-  movies = JSON.parse(localStorage.getItem('movieListToSave'));
+function game(){
+    let id1 = document.querySelector("#td1")
+    let id2 = document.querySelector("#td2")
+    let id3 = document.querySelector("#td3")
+    let id4 = document.querySelector("#td4")
+    let id5 = document.querySelector("#td5")
+    let id6 = document.querySelector("#td6")
+    let id7 = document.querySelector("#td7")
+    let id8 = document.querySelector("#td8")
+    let id9 = document.querySelector("#td9")
 
 
-  const titleInput = document.getElementById('title');
-  const genreInput = document.getElementById('genre');
-  const resultULTag = document.getElementById('results');
-  const countTag = document.getElementById('count');
+    //---------------1----------------//
+    id1.addEventListener("click",function(){
+        id1.textContent = "X"
+    })
+    id1.addEventListener("ondblclick",function(){
+        id1.textContent = "O"
 
-  let searchResults = [];
-
-  document.getElementById('search').addEventListener('click', function(event){
-    console.log("button clicked");
-
-    if(titleInput.value) {
-        searchResults = searchByTitle(titleInput.value);
-    } else if(genreInput.value) {
-        searchResults = searchByGenre(genreInput.value);
-    }
+    })
+        //---------------2----------------//
+        id2.addEventListener("click",function(){
+            id2.textContent = "X"
+        })
+        id2.addEventListener("ondblclick",function(){
+            id2.textContent = "O"
     
-    displayResults(searchResults);
-  });
-
-  
-
-  function searchByTitle(searchTerm) {
-    console.log("search by title");
-    // search by doing lower case on both sides and trimming the search term
-    return movies.filter(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase().trim()));
-  }
-
-  function searchByGenre(searchTerm) {
-    console.log("search by genre");
-    // search by doing lower case on both sides and trimming the search term
-    return movies.filter(movie => movie.genre.toLowerCase().includes(searchTerm.toLowerCase().trim()));
-  }
-
-  function displayResults(list) {
-    resultULTag.innerHTML = ''; // clearing the previous results
-    list.map(ele => {
-        let childTag = `<li>${ele.title}(${ele.genre})</li>`;
-        console.log(childTag);
-        resultULTag.innerHTML+= childTag;
     })
-    countByGenre(list);
-  }
-
-  function sortByTitle(){
-    console.log("sort by title called");
-    const sortedMovies = searchResults.sort((a,b)=> a.title.localeCompare(b.title));
-    displayResults(sortedMovies);
-  }
-
-  function sortByGenre(){
-    console.log("sort by genre called");
-    const sortedMovies = searchResults.sort((a,b)=> a.genre.localeCompare(b.genre));
-    displayResults(sortedMovies);
-  }
-
-  function countByGenre(list){
-    let countObject = {};
-    list.map(item => {
-        //alternative 1
-
-        // if(countObject[item.genre]) {
-        //     countObject[item.genre]++;
-        // } else {
-        //     countObject[item.genre] = 1;
-        // }
-
-        //alternative 2
-        countObject[item.genre] = (countObject[item.genre] | 0) + 1;
+        //---------------3----------------//
+        id3.addEventListener("click",function(){
+            id3.textContent = "X"
+        })
+        id3.addEventListener("ondblclick",function(){
+            id3.textContent = "O"
+    
+        })
+            //---------------4----------------//
+    id4.addEventListener("click",function(){
+        id4.textContent = "X"
     })
-    countTag.innerHTML = '';
-    for(key in countObject) {
-        console.log(key);
-        countTag.innerHTML += `<li>${key} : ${countObject[key]}</li>` 
-    }
+    id4.addEventListener("ondblclick",function(){
+        id4.textContent = "O"
 
-  }
+    })
+        //---------------5----------------//
+        id5.addEventListener("click",function(){
+            id5.textContent = "X"
+        })
+        id5.addEventListener("ondblclick",function(){
+            id5.textContent = "O"
+    
+        })
+            //---------------6----------------//
+    id6.addEventListener("click",function(){
+        id6.textContent = "X"
+    })
+    id6.addEventListener("ondblclick",function(){
+        id6.textContent = "O"
 
-  function searchBoth(title, genre){
-    return movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase().trim()) && movie.genre.toLowerCase().includes(genre.toLowerCase().trim()));
-  }
+    })
+        //---------------7----------------//
+        id7.addEventListener("click",function(){
+            id7.textContent = "X"
+        })
+        id7.addEventListener("ondblclick",function(){
+            id7.textContent = "O"
+    
+        })
+        //---------------8----------------//
+     id8.addEventListener("click",function(){
+            id8.textContent = "X"
+        })
+        id8.addEventListener("ondblclick",function(){
+            id8.textContent = "O"
+    
+        })
+         //---------------9----------------//
+    id8.addEventListener("click",function(){
+        id9.textContent = "X"
+    })
+    id9.addEventListener("ondblclick",function(){
+        id9.textContent = "O"
 
+    })
+
+    
+
+    
+
+    
+    
+
+}
